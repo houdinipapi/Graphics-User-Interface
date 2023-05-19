03557ee7bfbc039615596209715d851340438517
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-19 21:07:01 +0300

    Basic GUI using tkinter

--- Basic Graphics/basic_graphics.py
+++ Basic Graphics/basic_graphics.py
@@ -6,7 +6,7 @@ screen.title("My First Graphics Program")
 screen.geometry("500x500")
 
 # Welcome Text
-welcome_text = Label(text="Welcome to my first graphics program.")
+welcome_text = Label(text="Welcome to my first graphics program.", fg="green", bg="light blue")
 welcome_text.pack()
 
 screen.mainloop()
