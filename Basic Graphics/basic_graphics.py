834d11088552cff8b9186f2796921e47991714c8
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-19 21:15:05 +0300

    Basic GUI using tkinter

--- Basic Graphics/basic_graphics.py
+++ Basic Graphics/basic_graphics.py
@@ -18,4 +18,8 @@ welcome_text.pack()
 click_me = Button(text="Click Me", fg="red", bg="light green", command=run)  # --> add (height=10, width=20) for more modification.
 click_me.place(x=10, y=20)
 
+# On clicking,text appears on screen
+new_text = Label(text="Hey, you clicked me!!", fg="red", bg="yellow")
+new_text.pack
+
 screen.mainloop()
