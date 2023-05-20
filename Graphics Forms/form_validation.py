dca11b57b3fdd4443b10af1309bfce347200e2ad
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-20 21:00:56 +0300

    Basic Graphics - Form Validation

--- Graphics Forms/form_validation.py
+++ Graphics Forms/form_validation.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+
+def register():
+    username_text = username_var.get()
+    password_text = password_var.get()
+
+
 # Screen initialization
 screen = Tk()
 screen.geometry("500x500")
@@ -25,5 +31,9 @@ username_entry.place(x=15, y=80)
 
 password_entry = Entry(screen, textvariable=password_var, width=30, show="*")
 password_entry.place(x=15, y=150)
+
+# Validation Button
+register_btn = Button(screen, text="REGISTER", width=10, fg="black", bg="light green", command=register)
+register_btn.place(x=15, y=210)
 
 screen.mainloop()
