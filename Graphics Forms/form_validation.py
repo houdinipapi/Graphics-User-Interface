d9565f8727450ea239be88f341db7c18c6a28c0c
Houdini Papi <ochiengcliff.co@gmail.com>
2023-05-20 20:35:51 +0300

    Basic Graphics - Form Validation

--- Graphics Forms/form_validation.py
+++ Graphics Forms/form_validation.py
@@ -5,3 +5,8 @@ screen = Tk()
 screen.geometry("500x500")
 screen.title("Form Validation")
 
+# Heading
+heading = Label(text="Python Form Validation", fg="black", bg="grey", width=500, height=2)
+heading.pack()
+
+screen.mainloop()
